63cf584417e38a48aa0aa385cb3d0be11b67939f
SiamBhau <smd440046@gmail.com>
2025-06-04 14:38:24 +0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,6 @@ SERVERS = {
     "EUROPE": os.getenv("EUROPE_SERVER", "https://clientbp.ggblueshark.com"),
     "IND": os.getenv("IND_SERVER", "https://client.ind.freefiremobile.com"),
     "BR": os.getenv("BR_SERVER", "https://client.us.freefiremobile.com"),
-    "BD": os.gentenv(BD_SERVER", "https://clientbp.ggblueshark.com/LikeProfile"),
 }
 
 
